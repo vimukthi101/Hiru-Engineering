176d064dda0c3e3035d1acc50a775dce192c587c
unknown <vimukthisaranga1@gmail.com>
2017-11-20 22:39:13 +0530

    initial commit

---
+++ map/googleMaps.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+var myCenter=new google.maps.LatLng(6.808845,79.930604);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
